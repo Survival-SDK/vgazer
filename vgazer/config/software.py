b876ab30e8dc94671587c0c5a4a48f8965f52998
Vasiliy Edomin <Vasiliy.Edomin@gmail.com>
2019-06-28 04:38:14 +0600

    Add software data config

---
+++ vgazer/config/software.py
@@ -0,0 +1,345 @@
+config = {
+    "bzip2": {
+        "type": "library",
+        "platform": "specific",
+        "projects": {
+            "sourceforge": "bzip2",
+            "debian": "bzip2",
+        },
+    },
+    "cjson": {
+        "type": "library",
+        "platform": "specific",
+        "projects": {
+            "github": "cjson",
+            "debian": "cjson",
+        },
+    },
+    "cmocka": {
+        "type": "library",
+        "platform": "specific",
+        "projects": {
+            "custom": "cmocka",
+            "custom": "cmocka",
+        },
+    },
+    "dr_wav": {
+        "type": "library",
+        "platform": "specific",
+        "projects": {
+            "custom": "dr_wav",
+        },
+    },
+    "duktape": {
+        "type": "library",
+        "platform": "specific",
+        "projects": {
+            "github": "duktape",
+            "debian": "duktape",
+        },
+    },
+    "freetype": {
+        "type": "library",
+        "platform": "specific",
+        "projects": {
+            "custom": "freetype",
+            "debian": "freetype",
+        },
+    },
+    "giflib": {
+        "type": "library",
+        "platform": "specific",
+        "projects": {
+            "sourceforge": "giflib",
+            "debian": "giflib",
+        },
+    },
+    "glew": {
+        "type": "library",
+        "platform": "specific",
+        "projects": {
+            "sourceforge": "glew",
+            "debian": "glew",
+        },
+    },
+    "glib": {
+        "type": "library",
+        "platform": "specific",
+        "projects": {
+            "custom": "glib",
+            "debian": "glib2.0",
+        },
+    },
+    "harfbuzz": {
+        "type": "library",
+        "platform": "specific",
+        "projects": {
+            "github": "harfbuzz",
+            "debian": "harfbuzz",
+        },
+    },
+    "icu": {
+        "type": "library",
+        "platform": "specific",
+        "projects": {
+            "custom": "icu",
+            "debian": "icu",
+        },
+    },
+    "inih": {
+        "type": "library",
+        "platform": "specific",
+        "projects": {
+            "github": "inih",
+            "debian": "libinih",
+        },
+    },
+    "jpeg": {
+        "type": "library",
+        "platform": "specific",
+        "projects": {
+            "custom": "jpeg",
+            "debian": "libjpeg-turbo",
+        },
+    },
+    "lazy-winapi.c": {
+        "type": "library",
+        "platform": "specific",
+        "projects": {
+            "github": "lazy-winapi.c",
+        },
+    },
+    "libffi": {
+        "type": "library",
+        "platform": "specific",
+        "projects": {
+            "custom": "libffi",
+            "debian": "libffi",
+        },
+    },
+    "libflac": {
+        "type": "library",
+        "platform": "specific",
+        "projects": {
+            "xiph": "libflac",
+            "debian": "flac",
+        },
+    },
+    "libiconv": {
+        "type": "library",
+        "platform": "specific",
+        "projects": {
+            "custom": "libiconv",
+        },
+    },
+    "libintl-lite": {
+        "type": "library",
+        "platform": "specific",
+        "projects": {
+            "github": "libintl-lite",
+        },
+    },
+    "liblzma": {
+        "type": "library",
+        "platform": "specific",
+        "projects": {
+            "sourceforge": "lzmautils",
+            "debian": "xz-utils",
+        },
+    },
+    "libmodplug": {
+        "type": "library",
+        "platform": "specific",
+        "projects": {
+            "sourceforge": "modplug-xmms",
+            "debian": "libmodplug",
+        },
+    },
+    "libogg": {
+        "type": "library",
+        "platform": "specific",
+        "projects": {
+            "xiph": "libogg",
+            "debian": "libogg",
+        },
+    },
+    "libpng": {
+        "type": "library",
+        "platform": "specific",
+        "projects": {
+            "sourceforge": "libpng",
+            "debian": "libpng1.6",
+        },
+    },
+    "libtiff": {
+        "type": "library",
+        "platform": "specific",
+        "projects": {
+            "custom": "libtiff",
+            "debian": "tiff",
+        },
+    },
+    "libvorbis": {
+        "type": "library",
+        "platform": "specific",
+            "xiph": "libvorbis",
+            "debian": "libvorbis",
+    },
+    "libwebp": {
+        "type": "library",
+        "platform": "specific",
+        "projects": {
+            "custom": "libwebp",
+            "debian": "libwebp",
+        },
+    },
+    "lua": {
+        "type": "library",
+        "platform": "specific",
+        "projects": {
+            "custom": "lua",
+            "debian": "lua5.3",
+        },
+    },
+    "minini": {
+        "type": "library",
+        "platform": "specific",
+            "github": "minini",
+            "debian": "libminini",
+        },
+    },
+    "mpg123": {
+        "type": "library",
+        "platform": "specific",
+        "projects": {
+            "sourceforge": "mpg123",
+            "debian": "mpg123",
+        },
+    },
+    "nuklear": {
+        "type": "library",
+        "platform": "specific",
+        "projects": {
+            "github": "nuklear",
+        },
+    },
+    "p7": {
+        "type": "library",
+        "platform": "specific",
+        "projects": {
+            "custom": "p7",
+        },
+    },
+    "physfs": {
+        "type": "library",
+        "platform": "specific",
+            "github": "physfs",
+            "debian": "libphysfs",
+        },
+    },
+    "portaudio": {
+        "type": "library",
+        "platform": "specific",
+        "projects": {
+            "custom": "portaudio",
+            "debian": "portaudio19",
+        },
+    },
+    "saneopt": {
+        "type": "library",
+        "platform": "specific",
+        "projects": {
+            "github": "saneopt",
+        },
+    },
+    "sdl2": {
+        "type": "library",
+        "platform": "specific",
+        "projects": {
+            "custom": "sdl2",
+            "debian": "libsdl2",
+        },
+    },
+    "sdl2_gfx": {
+        "type": "library",
+        "platform": "specific",
+        "projects": {
+            "sourceforge": "sdl2gfx",
+            "debian": "libsdl2-gfx",
+        },
+    }
+    "sdl2_gpu": {
+        "type": "library",
+        "platform": "specific",
+        "projects": {
+            "github": "sdl-gpu",
+        },
+    },
+    "sdl2_image": {
+        "type": "library",
+        "platform": "specific",
+            "custom": "sdl2_image",
+            "debian": "libsdl2-image",
+        },
+    },
+    "sdl2_mixer": {
+        "type": "library",
+        "platform": "specific",
+        "projects": {
+            "custom": "sdl2_mixer",
+            "debian": "libsdl2-mixer",
+        },
+    },
+    "sdl2_ttf": {
+        "type": "library",
+        "platform": "specific",
+        "projects": {
+            "custom": "sdl2_ttf",
+            "debian": "libsdl2-ttf",
+        },
+    },
+    "squirrel": {
+        "type": "library",
+        "platform": "specific",
+        "projects": {
+            "github": "squirrel",
+            "debian": "squirrel3",
+    },
+    "std_rect_pack": {
+        "type": "library",
+        "platform": "specific",
+        "projects": {
+            "custom": "stb_rect_pack",
+            "debian": "libstb",
+        },
+    },
+    "tinyfiledialogs": {
+        "type": "library",
+        "platform": "specific",
+        "projects": {
+            "custom": "tinyfiledialogs",
+        },
+    },
+    "utf": {
+        "type": "library",
+        "platform": "specific",
+        "projects": {
+            "github": "utf",
+        },
+    },
+    "utf8": {
+        "type": "library",
+        "platform": "specific",
+        "projects": {
+            "github": "utf8",
+        },
+    },
+    "zlib": {
+        "type": "library",
+        "platform": "specific",
+        "projects": {
+            "custom": "zlib",
+            "debian": "zlib",
+        },
+    },
+}
