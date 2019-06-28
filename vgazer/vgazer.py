d35efb3144ab852e29fc31c04de678aeb79808e2
Vasiliy Edomin <Vasiliy.Edomin@gmail.com>
2019-06-28 21:54:13 +0600

    Add main Vgazer class and some extra exceptions classes

---
+++ vgazer/vgazer.py
@@ -0,0 +1,69 @@
+from vgazer.auth.base               import AuthBase
+from vgazer.auth.github             import AuthGithub
+from vgazer.config.github_projects  import ConfigGithubProjects
+from vgazer.config.software         import ConfigSoftware
+from vgazer.platform                import Platform
+from vgazer.version.custom          import CheckCustom
+from vgazer.version.github          import CheckGithub
+from vgazer.version.sourceforge     import CheckSourceforge
+from vgazer.version.xiph            import CheckXiph
+from vgazer.version.debian          import CheckDebian
+from vgazer.version.debian          import DebianPackageUnavailable
+
+class UnknownSoftware(Exception):
+    def __init__(self, message):
+        super().__init__(message)
+
+class Vgazer:
+    def __init__(self, arch = None, os = None, osVersion = None,
+     compiler = None, customGithubProjectsData = {}, customSoftwareData = {}):
+        self.authBase = AuthBase()
+        self.authGithub = AuthGithub()
+        self.configGithubProjects = ConfigGithubProjects(
+         customGithubProjectsData)
+        self.configSoftware = ConfigSoftware(customSoftwareData)
+        self.hostPlatform = Platform()
+        self.targetPlatform = Platform(arch, os, osVersion, compiler)
+
+    def CheckVersionCrossplatform(self, software):
+        softwareData = self.configSoftware.GetData()
+        projects = softwareData[software]["projects"]
+        if "github" in projects:
+            return CheckGithub(self.authGithub, self.configGithubProjects,
+             projects["github"])
+        elif "sourceforge" in projects:
+            return CheckSourceforge(self.authBase, projects["sourceforge"])
+        elif "xiph" in projects:
+            return CheckXiph(self.authBase, projects["xiph"])
+        elif "custom" in projects:
+            return CheckCustom(self.authBase, projects["custom"])
+
+    def CheckVersionDebian(self, software, debianVersion):
+        softwareData = self.configSoftware.GetData()
+        projects = softwareData[software]["projects"]
+        if "debian" not in projects:
+            return self.CheckVersionCrossplatform(software)
+        try:
+            return CheckDebian(self.authBase, debianVersion, projects["debian"])
+        except DebianPackageUnavailable:
+            return self.CheckVersionCrossplatform(software)
+
+    def CheckVersion(self, software):
+        crossplatform = not self.targetPlatform.PlatformsEqual(
+         self.hostPlatform)
+        softwareData = self.configSoftware.GetData()
+        if software not in softwareData:
+            raise GithubApiRateLimitExceeded("Unknown software: " + software)
+
+        if crossplatform:
+            return self.CheckVersionCrossplatform(software)
+        elif self.targetPlatform.GetOs() == "debian":
+            return self.CheckVersionDebian(software,
+             self.targetPlatform.GetOsVersion())
+
+    #def AddGithubProjectsData(self, data):
+        #self.configGithubProjects.AddData(data)
+
+    #def AddSoftwareData(self, data):
+        #self.ConfigSoftware.AddData(data)
+
