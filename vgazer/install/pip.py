623caa42993afcec0e4cfff5d9393b323641c0a6
Vasiliy Edomin <Vasiliy.Edomin@gmail.com>
2019-08-16 22:11:46 +0600

    Add pip installer

---
+++ vgazer/install/pip.py
@@ -0,0 +1,12 @@
+from vgazer.command     import RunCommand
+from vgazer.exceptions  import CommandError
+from vgazer.exceptions  import InstallError
+
+def InstallPip(software, package, verbose):
+    try:
+        RunCommand(["pip", "install", package, "--no-cache-dir"], verbose)
+    except CommandError:
+        print("Unable to install", software)
+        raise InstallError(software + " not installed")
+
+    print(software, "installed")
