e4b7857a33e354767339f5c6928903a23735ed73
Vasiliy Edomin <Vasiliy.Edomin@gmail.com>
2019-08-16 22:11:22 +0600

    add PyPI version checker

---
+++ vgazer/version/pypi.py
@@ -0,0 +1,7 @@
+import requests
+from bs4 import BeautifulSoup
+
+def CheckPypi(auth, project):
+    projectInfo = auth.GetJson("https://pypi.org/pypi/" + project + "/json")
+
+    return projectInfo["info"]["version"]
