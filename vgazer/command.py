127f49b0a23ace947aaafd168e0c6f0bad8e0ae1
Vasiliy Edomin <Vasiliy.Edomin@gmail.com>
2019-08-16 14:38:09 +0600

    Add function for running command and returning output

--- vgazer/command.py
+++ vgazer/command.py
@@ -14,3 +14,10 @@ def RunCommand(command, verbose):
         print ("Error occured while running utility:", command[0])
         raise CommandError("Error occured while running utility:" + command[0])
 
+def GetCommandOutputUtf8(command):
+    try:
+        return subprocess.check_output(
+         command, stderr=subprocess.STDOUT
+        ).decode("utf-8")
+    except subprocess.CalledProcessError:
+        raise CommandError("Error occured while running utility:" + command[0])
