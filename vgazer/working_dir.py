fd8e148703a0781b521c3f336448ee58178b5bf9
Vasiliy Edomin <Vasiliy.Edomin@gmail.com>
2019-08-16 14:42:52 +0600

    Add WorkingDir class

---
+++ vgazer/working_dir.py
@@ -0,0 +1,15 @@
+# https://stackoverflow.com/questions/431684/how-do-i-change-directory-cd-in-python
+
+import os
+
+class WorkingDir:
+    """Context manager for changing the current working directory"""
+    def __init__(self, newPath):
+        self.newPath = os.path.expanduser(newPath)
+
+    def __enter__(self):
+        self.savedPath = os.getcwd()
+        os.chdir(self.newPath)
+
+    def __exit__(self, etype, value, traceback):
+        os.chdir(self.savedPath)
