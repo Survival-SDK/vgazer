a408faa7491343c185a9137a39c663c3ea96fc63
Vasiliy Edomin <Vasiliy.Edomin@gmail.com>
2019-08-16 14:43:10 +0600

    Add wget custom version checker

---
+++ vgazer/version/custom_checker/wget.py
@@ -0,0 +1,16 @@
+import requests
+from bs4 import BeautifulSoup
+
+def Check(auth):
+    response = requests.get("https://ftp.gnu.org/gnu/wget/")
+    html = response.content.decode("utf-8")
+    parsedHtml = BeautifulSoup(html, "html.parser")
+
+    table = parsedHtml.table
+    rows = table.findChildren("tr")
+    lastVersionRow = rows[-8]
+    link = lastVersionRow.findChildren("a")
+
+    version = link[0].text.split("-")[1].split(".tar.gz")[0]
+
+    return version
