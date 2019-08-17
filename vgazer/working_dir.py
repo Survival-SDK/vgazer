57183584d9e2125f0d7224556b7d6d387d60ac19
Vasiliy Edomin <Vasiliy.Edomin@gmail.com>
2019-08-17 12:45:58 +0600

    add message when changing dir with WorkingDir()

--- vgazer/working_dir.py
+++ vgazer/working_dir.py
@@ -9,6 +9,7 @@ class WorkingDir:
 
     def __enter__(self):
         self.savedPath = os.getcwd()
+        print("Entering to directory:", self.newPath)
         os.chdir(self.newPath)
 
     def __exit__(self, etype, value, traceback):
