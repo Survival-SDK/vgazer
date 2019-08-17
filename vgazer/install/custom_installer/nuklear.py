910786168c58b483e130f39299402db3bad91fff
Vasiliy Edomin <Vasiliy.Edomin@gmail.com>
2019-08-17 13:37:04 +0600

    add nuklear installer

---
+++ vgazer/install/custom_installer/nuklear.py
@@ -0,0 +1,21 @@
+import os
+
+from vgazer.command     import RunCommand
+from vgazer.exceptions  import CommandError
+from vgazer.exceptions  import InstallError
+from vgazer.platform    import GetInstallPrefix
+
+def Install(auth, software, platform, platformData, verbose):
+    installPrefix = GetInstallPrefix(platformData)
+
+    url = "https://raw.githubusercontent.com/vurtun/nuklear/master/nuklear.h"
+
+    try:
+        if not os.path.exists(installPrefix + "/include"):
+            RunCommand(["mkdir", "-p", installPrefix + "/include"], verbose)
+        RunCommand(["wget", "-P", installPrefix + "/include", url], verbose)
+    except CommandError:
+        print("Unable to install", software)
+        raise InstallError(software + " not installed")
+
+    print(software, "installed")
