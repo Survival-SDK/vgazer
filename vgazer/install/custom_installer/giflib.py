1ebec0f736e7cd801655fac816b94f25c7beebe4
Vasiliy Edomin <Vasiliy.Edomin@gmail.com>
2019-08-18 16:53:39 +0600

    add giflib installer

---
+++ vgazer/install/custom_installer/giflib.py
@@ -0,0 +1,41 @@
+import os
+import requests
+
+from vgazer.command     import RunCommand
+from vgazer.exceptions  import CommandError
+from vgazer.exceptions  import InstallError
+from vgazer.exceptions  import TarballLost
+from vgazer.platform    import GetInstallPrefix
+from vgazer.store.temp  import StoreTemp
+from vgazer.working_dir import WorkingDir
+
+def Install(auth, software, platform, platformData, verbose):
+    installPrefix = GetInstallPrefix(platformData)
+
+    storeTemp = StoreTemp()
+    storeTemp.ResolveEmptySubdirectory(software)
+    tempPath = storeTemp.GetSubdirectoryPath(software)
+
+    tarballUrl = requests.get(
+     "https://sourceforge.net/projects/giflib/best_release.json"
+    ).json()["release"]["url"]
+    tarballShortFilename = tarballUrl.split("/")[-2]
+
+    try:
+        with WorkingDir(tempPath):
+            RunCommand(
+             ["wget", "-P", "./", "-O", tarballShortFilename, tarballUrl],
+             verbose)
+            RunCommand(
+             ["tar", "--verbose", "--extract", "--gzip", "--file",
+              tarballShortFilename],
+             verbose)
+        extractedDir = os.path.join(tempPath, tarballShortFilename[0:-7])
+        with WorkingDir(extractedDir):
+            RunCommand(["make"], verbose)
+            RunCommand(["make", "install", "PREFIX=" + installPrefix], verbose)
+    except CommandError:
+        print("Unable to install", software)
+        raise InstallError(software + " not installed")
+
+    print(software, "installed")
