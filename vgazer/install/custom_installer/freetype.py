7052b6f83747f0c551386a335c62e8436bbe29c9
Vasiliy Edomin <Vasiliy.Edomin@gmail.com>
2019-08-18 16:25:15 +0600

    add freetype installer

---
+++ vgazer/install/custom_installer/freetype.py
@@ -0,0 +1,44 @@
+import os
+import requests
+
+from vgazer.command     import RunCommand
+from vgazer.exceptions  import CommandError
+from vgazer.exceptions  import InstallError
+from vgazer.exceptions  import TarballLost
+from vgazer.platform    import GetInstallPrefix
+from vgazer.store.temp  import StoreTemp
+from vgazer.working_dir import WorkingDir
+
+def Install(auth, software, platform, platformData, verbose):
+    installPrefix = GetInstallPrefix(platformData)
+
+    storeTemp = StoreTemp()
+    storeTemp.ResolveEmptySubdirectory(software)
+    tempPath = storeTemp.GetSubdirectoryPath(software)
+
+    tarballUrl = requests.get(
+     "https://sourceforge.net/projects/freetype/best_release.json"
+    ).json()["release"]["url"]
+    tarballShortFilename = tarballUrl.split("/")[-2]
+
+    try:
+        with WorkingDir(tempPath):
+            RunCommand(["wget", "-P", "./", "-O", tarballShortFilename, tarballUrl], verbose)
+            RunCommand(
+             ["tar", "--verbose", "--extract", "--gzip", "--file",
+              tarballShortFilename],
+             verbose)
+        extractedDir = os.path.join(tempPath, tarballShortFilename[0:-7])
+        with WorkingDir(extractedDir):
+            RunCommand(
+             ["./configure", "--prefix=" + installPrefix,
+              "BZIP2_CFLAGS=-I" + installPrefix + "/include",
+              "BZIP2_LIBS=-L" + installPrefix + "/lib -lbz2"],
+             verbose)
+            RunCommand(["make"], verbose)
+            RunCommand(["make", "install"], verbose)
+    except CommandError:
+        print("Unable to install", software)
+        raise InstallError(software + " not installed")
+
+    print(software, "installed")
