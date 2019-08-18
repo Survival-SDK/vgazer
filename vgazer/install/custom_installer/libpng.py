05b8bf88f548588e4110ba93797efeae88c1ca9d
Vasiliy Edomin <Vasiliy.Edomin@gmail.com>
2019-08-18 14:58:11 +0600

    add libpng installer

---
+++ vgazer/install/custom_installer/libpng.py
@@ -0,0 +1,53 @@
+import os
+import requests
+from bs4 import BeautifulSoup
+
+from vgazer.command     import RunCommand
+from vgazer.exceptions  import CommandError
+from vgazer.exceptions  import InstallError
+from vgazer.exceptions  import TarballLost
+from vgazer.platform    import GetInstallPrefix
+from vgazer.store.temp  import StoreTemp
+from vgazer.working_dir import WorkingDir
+
+def GetTarballUrl():
+    response = requests.get("http://www.libpng.org/pub/png/libpng.html")
+    html = response.content.decode("utf-8")
+    parsedHtml = BeautifulSoup(html, "html.parser")
+
+    links = parsedHtml.find_all("a")
+    for link in links:
+        if link["href"].endswith("tar.gz"):
+            return link["href"]
+
+def Install(auth, software, platform, platformData, verbose):
+    installPrefix = GetInstallPrefix(platformData)
+
+    storeTemp = StoreTemp()
+    storeTemp.ResolveEmptySubdirectory(software)
+    tempPath = storeTemp.GetSubdirectoryPath(software)
+
+    tarballUrl = GetTarballUrl()
+    tarballShortFilename = tarballUrl.split("/")[-1]
+
+    try:
+        with WorkingDir(tempPath):
+            RunCommand(["wget", "-P", "./", tarballUrl], verbose)
+            RunCommand(
+             ["tar", "--verbose", "--extract", "--gzip", "--file",
+              tarballShortFilename],
+             verbose)
+        extractedDir = os.path.join(tempPath, tarballShortFilename[0:-7])
+        with WorkingDir(extractedDir):
+            RunCommand(
+             ["./configure", "--prefix=" + installPrefix,
+              "CPPFLAGS=-I" + installPrefix + "/include",
+              "LDFLAGS=-L" + installPrefix + "/lib"],
+             verbose)
+            RunCommand(["make"], verbose)
+            RunCommand(["make", "install"], verbose)
+    except CommandError:
+        print("Unable to install", software)
+        raise InstallError(software + " not installed")
+
+    print(software, "installed")
