efe02af0c5a15a2b3b47cbee247aa3d220eebfdc
Vasiliy Edomin <Vasiliy.Edomin@gmail.com>
2019-08-18 14:58:00 +0600

    fix codestyle in zlib installer

--- vgazer/install/custom_installer/zlib.py
+++ vgazer/install/custom_installer/zlib.py
@@ -2,13 +2,13 @@ import os
 import requests
 from bs4 import BeautifulSoup
 
-from vgazer.command         import RunCommand
-from vgazer.exceptions      import CommandError
-from vgazer.exceptions      import InstallError
-from vgazer.exceptions      import TarballLost
-from vgazer.platform        import GetInstallPrefix
-from vgazer.store.temp      import StoreTemp
-from vgazer.working_dir     import WorkingDir
+from vgazer.command     import RunCommand
+from vgazer.exceptions  import CommandError
+from vgazer.exceptions  import InstallError
+from vgazer.exceptions  import TarballLost
+from vgazer.platform    import GetInstallPrefix
+from vgazer.store.temp  import StoreTemp
+from vgazer.working_dir import WorkingDir
 
 def GetTarballUrl():
     response = requests.get("http://zlib.net/")
