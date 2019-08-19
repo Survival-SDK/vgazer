6c37ead9825da958f0a07d93e5d26cc5bbbb11d9
Vasiliy Edomin <Vasiliy.Edomin@gmail.com>
2019-08-19 21:10:13 +0600

    Add libmount custom installer

---
+++ vgazer/install/custom_installer/libmount.py
@@ -0,0 +1,57 @@
+import os
+import requests
+
+from vgazer.command         import GetCommandOutputUtf8
+from vgazer.command         import RunCommand
+from vgazer.exceptions      import CommandError
+from vgazer.exceptions      import GithubApiRateLimitExceeded
+from vgazer.exceptions      import InstallError
+from vgazer.github_common   import GithubCheckApiRateLimitExceeded
+from vgazer.platform        import GetInstallPrefix
+from vgazer.store.temp      import StoreTemp
+from vgazer.working_dir     import WorkingDir
+
+def Install(auth, software, platform, platformData, verbose):
+    installPrefix = GetInstallPrefix(platformData)
+
+    storeTemp = StoreTemp()
+    storeTemp.ResolveEmptySubdirectory(software)
+    tempPath = storeTemp.GetSubdirectoryPath(software)
+
+    tags = auth["github"].GetJson(
+     "https://api.github.com/repos/karelzak/util-linux/tags")
+
+    if GithubCheckApiRateLimitExceeded(tags):
+        raise GithubApiRateLimitExceeded(
+         "Github API rate limit reached while searching last version of "
+         "repo: karelzak/util-linux"
+        )
+
+    tarballUrl = tags[0]["tarball_url"]
+    tarballShortFilename = tarballUrl.split("/")[-1]
+
+    try:
+        with WorkingDir(tempPath):
+            RunCommand(["wget", "-P", "./", tarballUrl], verbose)
+            RunCommand(
+             ["tar", "--verbose", "--extract", "--gzip", "--file",
+              tarballShortFilename],
+             verbose)
+            output = GetCommandOutputUtf8(
+             ["tar", "--list", "--file", tarballShortFilename]
+            )
+        extractedDir = os.path.join(tempPath, output.splitlines()[0].split("/")[0])
+        with WorkingDir(extractedDir):
+            RunCommand(["./autogen.sh"], verbose)
+            RunCommand(
+             ["./configure", "--prefix=" + installPrefix, "--disable-static",
+              "--disable-all-programs", "--enable-libblkid", "--enable-libmount"
+             ],
+             verbose)
+            RunCommand(["make"], verbose)
+            RunCommand(["make", "install"], verbose)
+    except CommandError:
+        print("Unable to install", software)
+        raise InstallError(software + " not installed")
+
+    print(software, "installed")
