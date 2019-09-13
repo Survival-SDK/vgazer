509c14b71e4edf6490293db07dfa73bdb51a05a0
Vasiliy Edomin <Vasiliy.Edomin@gmail.com>
2019-09-13 20:37:18 +0600

    Fix getting version from release tarball filename

--- vgazer/version/utils.py
+++ vgazer/version/utils.py
@@ -6,6 +6,6 @@ def GetVersionFromTag(tag):
 def GetVersionFromFilename(filename):
     VersionAndExt = filename.split("-")[-1]
 
-    for i in range(0, 4):
+    for i in range(0, 5):
         if VersionAndExt.split(".")[i].isalpha():
             return ".".join(VersionAndExt.split(".")[0:i])
