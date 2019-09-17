374ecc677c0eb94dfadbaa6abcfeced6abde6d45
Vasiliy Edomin <Vasiliy.Edomin@gmail.com>
2019-09-18 02:05:41 +0600

    Add function PrintHostPlatformData for samples

--- samples/samples_common.py
+++ samples/samples_common.py
@@ -1,7 +1,10 @@
-def PrintPlatformData(gazer):
+def PrintHostPlatformData(gazer):
     print("host:", gazer.GetHostPlatform().GetArch(),
      gazer.GetHostPlatform().GetOs(), gazer.GetHostPlatform().GetOsVersion(),
      gazer.GetHostPlatform().GetAbi())
+
+def PrintPlatformData(gazer):
+    PrintHostPlatformData(gazer)
 
     print("target:", gazer.GetTargetPlatform().GetArch(),
      gazer.GetTargetPlatform().GetOs(),
