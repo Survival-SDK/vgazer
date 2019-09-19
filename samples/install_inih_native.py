c100f140641903841fea20de8289b5e3c5613b1d
Vasiliy Edomin <Vasiliy.Edomin@gmail.com>
2019-09-19 19:49:30 +0600

    Add inih custom installer and sample of native installation

---
+++ samples/install_inih_native.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+
+import os
+import sys
+import inspect
+
+currentFrame = inspect.currentframe()
+currentFile = os.path.abspath(inspect.getfile(currentFrame))
+currentDir = os.path.dirname(currentFile)
+parentDir = os.path.dirname(currentDir)
+
+sys.path.insert(0, parentDir)
+
+from vgazer.vgazer import Vgazer
+
+def main():
+    gazer = Vgazer()
+
+    print("host:", gazer.GetHostPlatform().GetArch(),
+     gazer.GetHostPlatform().GetOs(), gazer.GetHostPlatform().GetOsVersion(),
+     gazer.GetHostPlatform().GetAbi())
+    print("target:", gazer.GetTargetPlatform().GetArch(),
+     gazer.GetTargetPlatform().GetOs(),
+     gazer.GetTargetPlatform().GetOsVersion(),
+     gazer.GetTargetPlatform().GetAbi())
+
+    hostArch = gazer.GetTargetPlatform().GetArch()
+    hostOs = gazer.GetTargetPlatform().GetOs()
+    hostOsVersion = gazer.GetTargetPlatform().GetOsVersion()
+
+    if not (
+     (hostOs == "debian") and (hostOsVersion in ["buster", "bullseye", "sid"])
+    ):
+        if hostOs == "alpine":
+            gazer.Install(hostArch + "-linux-musl-gcc", verbose=True)
+            gazer.Install("musl", verbose=True)
+        if hostOs == "debian":
+            gazer.Install("wget", verbose=True)
+    gazer.Install("inih", verbose=True)
+
+if __name__ == "__main__":
+    main()
---
+++ vgazer/install/custom_installer/inih.py
@@ -0,0 +1,66 @@
+import os
+import requests
+
+from vgazer.command         import GetCommandOutputUtf8
+from vgazer.command         import RunCommand
+from vgazer.exceptions      import CommandError
+from vgazer.exceptions      import GithubApiRateLimitExceeded
+from vgazer.exceptions      import InstallError
+from vgazer.github_common   import GithubCheckApiRateLimitExceeded
+from vgazer.platform        import GetAr
+from vgazer.platform        import GetCc
+from vgazer.platform        import GetInstallPrefix
+from vgazer.store.temp      import StoreTemp
+from vgazer.working_dir     import WorkingDir
+
+def Install(auth, software, platform, platformData, verbose):
+    installPrefix = GetInstallPrefix(platformData)
+    ar = GetAr(platformData["target"])
+    cc = GetCc(platformData["target"])
+
+    storeTemp = StoreTemp()
+    storeTemp.ResolveEmptySubdirectory(software)
+    tempPath = storeTemp.GetSubdirectoryPath(software)
+
+    releases = auth["github"].GetJson(
+     "https://api.github.com/repos/benhoyt/inih/releases")
+
+    if GithubCheckApiRateLimitExceeded(releases):
+        raise GithubApiRateLimitExceeded(
+         "Github API rate limit reached while searching last version of "
+         "repo: benhoyt/inih"
+        )
+
+    tarballUrl = releases[0]["tarball_url"]
+    tarballShortFilename = tarballUrl.split("/")[-1]
+
+    try:
+        with WorkingDir(tempPath):
+            RunCommand(["wget", "-P", "./", tarballUrl], verbose)
+            RunCommand(
+             ["tar", "--verbose", "--extract", "--gzip", "--file",
+              tarballShortFilename],
+             verbose)
+            output = GetCommandOutputUtf8(
+             ["tar", "--list", "--file", tarballShortFilename]
+            )
+        extractedDir = os.path.join(tempPath, output.splitlines()[0].split("/")[0])
+        with WorkingDir(extractedDir):
+            RunCommand(
+             [cc, "-c", "ini.c", "-o", "ini.o", "-O2", "-Wall", "-fPIC"],
+             verbose)
+            RunCommand([cc, "-shared", "-s", "-o", "libinih.so", "ini.o"],
+             verbose)
+            RunCommand([ar, "rcs", "libinih.a", "ini.o"], verbose)
+            if not os.path.exists(installPrefix + "/include"):
+                RunCommand(["mkdir", "-p", installPrefix + "/include"], verbose)
+            if not os.path.exists(installPrefix + "/lib"):
+                RunCommand(["mkdir", "-p", installPrefix + "/lib"], verbose)
+            RunCommand(["cp", "./ini.h", installPrefix + "/include"], verbose)
+            RunCommand(["cp", "./libinih.a", installPrefix + "/lib"], verbose)
+            RunCommand(["cp", "./libinih.so", installPrefix + "/lib"], verbose)
+    except CommandError:
+        print("Unable to install", software)
+        raise InstallError(software + " not installed")
+
+    print(software, "installed")
