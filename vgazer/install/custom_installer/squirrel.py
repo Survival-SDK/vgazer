15073f79c7f7d6f9fb517278a6741b0fc72d799a
Vasiliy Edomin <Vasiliy.Edomin@gmail.com>
2019-09-22 01:46:04 +0600

    Add squirrel custom installer and native installation sample

---
+++ vgazer/install/custom_installer/squirrel.py
@@ -0,0 +1,77 @@
+import os
+import requests
+
+from vgazer.command         import GetCommandOutputUtf8
+from vgazer.command         import RunCommand
+from vgazer.env_vars        import SetEnvVar
+from vgazer.exceptions      import CommandError
+from vgazer.exceptions      import GithubApiRateLimitExceeded
+from vgazer.exceptions      import InstallError
+from vgazer.github_common   import GithubCheckApiRateLimitExceeded
+from vgazer.platform        import GetCc
+from vgazer.platform        import GetCxx
+from vgazer.platform        import GetInstallPrefix
+from vgazer.store.temp      import StoreTemp
+from vgazer.working_dir     import WorkingDir
+
+def Install(auth, software, platform, platformData, verbose):
+    installPrefix = GetInstallPrefix(platformData)
+    cc = GetCc(platformData["target"])
+    cxx = GetCxx(platformData["target"])
+
+    storeTemp = StoreTemp()
+    storeTemp.ResolveEmptySubdirectory(software)
+    tempPath = storeTemp.GetSubdirectoryPath(software)
+
+    releases = auth["github"].GetJson(
+     "https://api.github.com/repos/albertodemichelis/squirrel/releases")
+
+    if GithubCheckApiRateLimitExceeded(releases):
+        raise GithubApiRateLimitExceeded(
+         "Github API rate limit reached while searching last version of "
+         "repo: albertodemichelis/squirrel"
+        )
+
+    if len(releases) == 0:
+        releases = auth["github"].GetJson(
+         "https://api.github.com/repos/albertodemichelis/squirrel/tags")
+
+    if GithubCheckApiRateLimitExceeded(releases):
+        raise GithubApiRateLimitExceeded(
+         "Github API rate limit reached while searching last version of "
+         "repo: albertodemichelis/squirrel"
+        )
+
+    tarballUrl = releases[0]["tarball_url"]
+    tarballShortFilename = "squirrel.tar.gz"
+
+    try:
+        with WorkingDir(tempPath):
+            RunCommand(
+             ["wget", "-P", "./", "-O", tarballShortFilename, tarballUrl],
+             verbose)
+            RunCommand(
+             ["tar", "--verbose", "--extract", "--gzip", "--file",
+              tarballShortFilename],
+             verbose)
+            output = GetCommandOutputUtf8(
+             ["tar", "--list", "--file", tarballShortFilename]
+            )
+        extractedDir = os.path.join(tempPath,
+         output.splitlines()[0].split("/")[0])
+        with WorkingDir(extractedDir):
+            RunCommand(["mkdir", "build"], verbose)
+        buildDir = os.path.join(extractedDir, "build")
+        with WorkingDir(buildDir):
+            SetEnvVar("CC", cc)
+            SetEnvVar("CXX", cxx)
+            RunCommand(
+             ["cmake", "..", "-DCMAKE_INSTALL_PREFIX=" + installPrefix],
+             verbose)
+            RunCommand(["make"], verbose)
+            RunCommand(["make", "install"], verbose)
+    except CommandError:
+        print("Unable to install", software)
+        raise InstallError(software + " not installed")
+
+    print(software, "installed")
