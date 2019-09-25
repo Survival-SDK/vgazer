1c3c096ed9c3f34d4015d953290945ab46445fd6
Vasiliy Edomin <Vasiliy.Edomin@gmail.com>
2019-09-26 02:50:45 +0600

    Add classes for working with mirrors of various websites
    Currently implemented only ftp.gnu.org

---
+++ vgazer/mirrors/base.py
@@ -0,0 +1,74 @@
+import time
+
+from vgazer.mirrors.manager import MirrorsManager
+from vgazer.store.home      import StoreHome
+
+class MirrorsBase():
+    @staticmethod
+    def ResolveHomeDirsAndFiles(storeHome, siteName):
+        if not storeHome.ResolveSubdirectory(siteName):
+            return False
+
+        filesResolved = storeHome.ResolveSubdirectoryFile(siteName, "mirrors")
+
+        if not filesResolved:
+            return False
+        return True
+
+    @staticmethod
+    def MirrorsListToText(mirrorsList):
+        data = ""
+        data += str(time.time()) + "\n"
+        for protocol in mirrorsList:
+            for mirror in mirrorsList[protocol]:
+                data += protocol + " " + mirror + "\n"
+        return data
+
+    @staticmethod
+    def MirrorsListFromText(data):
+        mirrorsList = {"http": [], "https": [], "ftp": [], "rsync": []}
+        for line in data.splitlines()[1:]:
+            protocol = line.split(" ")[0]
+            mirror = line.split(" ")[1]
+            mirrorsList[protocol].append(mirror)
+        return mirrorsList
+
+    @staticmethod
+    def MirrorsDataOutOfTime(data):
+        return float(data.splitlines()[0]) + 86400 < time.time()
+
+    def __init__(self, siteName, getMirrorsList):
+        print("Checking mirrors list for " + siteName + "...")
+        self.storeHome = StoreHome()
+        if self.storeHome.ResolveDirectory():
+            if self.ResolveHomeDirsAndFiles(self.storeHome, siteName):
+                data = self.storeHome.SubdirectoryReadTextFile(siteName,
+                 "mirrors")
+                if data == "":
+                    print("Stored mirrors list for " + siteName + " not found")
+                    self.mirrorsList = getMirrorsList()
+                    data = self.MirrorsListToText(self.mirrorsList)
+                    self.storeHome.SubdirectoryWriteTextFile(siteName,
+                     "mirrors", data)
+                elif self.MirrorsDataOutOfTime(data):
+                    print(
+                     "Stored mirrors list for " + siteName + " out of date")
+                    self.mirrorsList = getMirrorsList(noFallback = True)
+                    if self.mirrorsList is None:
+                        self.mirrorsList = self.MirrorsListFromText(data)
+                    else:
+                        data = self.MirrorsListToText(self.mirrorsList)
+                        self.storeHome.SubdirectoryWriteTextFile(siteName,
+                         "mirrors", data)
+                else:
+                    print("Found up to date mirrors list for " + siteName)
+                    self.mirrorsList = self.MirrorsListFromText(data)
+            else:
+                print("Unable to load stored mirrors list for " + siteName)
+                self.mirrorsList = getMirrorsList()
+        else:
+            print("Unable to load stored mirrors list for " + siteName)
+            self.mirrorsList = getMirrorsList()
+
+    def CreateMirrorsManager(self, permittedProtocols):
+        return MirrorsManager(self.mirrorsList, permittedProtocols)
---
+++ vgazer/mirrors/manager.py
@@ -0,0 +1,33 @@
+from vgazer.exceptions import NoSuitableMirrors
+
+class MirrorsManager():
+    def __init__(self, mirrorsList, permittedProtocols):
+        self.mirrorsList = mirrorsList
+        self.permittedProtocols = permittedProtocols
+        for protocol in permittedProtocols:
+            if len(mirrorsList[protocol]) == 0:
+                self.PermittedProtocols.remove(protocol)
+        if len(self.permittedProtocols) == 0:
+            raise NoSuitableMirrors(
+             "No suitable mirrors for permitted protocols")
+        self.currentProtocol = self.permittedProtocols[0]
+        self.currentMirrorIndex = 0
+
+    def GetMirrorUrl(self):
+        return self.mirrorsList[self.currentProtocol][self.currentMirrorIndex]
+
+    def ChangeMirror(self):
+        if self.currentMirrorIndex < len(self.mirrorsList[self.currentProtocol]):
+            self.currentMirrorIndex += 1
+        else:
+            protocolIndex = self.permittedProtocols.index[self.currentProtocol]
+            if protocolIndex < len(self.PermittedProtocols):
+                self.currentProtocol = self.PermittedProtocols[protocolIndex + 1]
+                self.currentMirrorIndex = 0
+            else:
+                raise NoSuitableMirrors(
+                 "No suitable mirrors for permitted protocols")
+
+    def GetNewMirrorUrl(self):
+        self.ChangeMirror()
+        return self.mirrorsList[self.currentProtocol][self.currentMirrorIndex]
