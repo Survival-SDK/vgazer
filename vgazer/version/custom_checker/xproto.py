31bd9f36050808e561efec2251577f7eb7d38ca1
Vasiliy Edomin <Vasiliy.Edomin@gmail.com>
2019-09-30 03:53:27 +0600

    Add custom checkers and custom installers for xproto, xextproro and xtrans

---
+++ vgazer/version/custom_checker/xproto.py
@@ -0,0 +1,36 @@
+import requests
+from bs4 import BeautifulSoup
+
+def Check(auth):
+    response = requests.get("https://www.x.org/releases/individual/proto/")
+    html = response.content.decode("utf-8")
+    parsedHtml = BeautifulSoup(html, "html.parser")
+
+    links = parsedHtml.find_all("a")
+
+    maxVersionMajor = -1
+    maxVersionMinor = -1
+    maxVersionPatch = -1
+    for link in links:
+        if ("xproto-" in link.text and not "dmxproto-" in link.text
+         and ".tar.gz" in link.text and ".sig" not in link.text):
+            version = link.text.split("-")[1].split(".tar.gz")[0].split(".")
+            versionMajor = int(version[0])
+            versionMinor = int(version[1])
+            versionPatch = int(version[2])
+
+            if versionMajor > maxVersionMajor:
+                maxVersionMajor = versionMajor
+                maxVersionMinor = versionMinor
+                maxVersionPatch = versionPatch
+            elif (versionMajor == maxVersionMajor
+             and versionMinor > maxVersionMinor):
+                maxVersionMinor = versionMinor
+                maxVersionPatch = versionPatch
+            elif (versionMajor == maxVersionMajor
+             and versionMinor == maxVersionMinor
+             and versionPatch > maxVersionPatch):
+                maxVersionPatch = versionPatch
+
+    return (str(versionMajor) + "." + str(maxVersionMinor) + "."
+     + str(maxVersionPatch))
---
+++ vgazer/version/custom_checker/xtrans.py
@@ -0,0 +1,41 @@
+import requests
+from bs4 import BeautifulSoup
+
+def Check(auth):
+    response = requests.get("https://www.x.org/releases/individual/lib/")
+    html = response.content.decode("utf-8")
+    parsedHtml = BeautifulSoup(html, "html.parser")
+
+    links = parsedHtml.find_all("a")
+
+    maxVersionMajor = -1
+    maxVersionMinor = -1
+    maxVersionPatch = -1
+    for link in links:
+        if ("xtrans-" in link.text and ".tar.gz" in link.text
+         and ".sig" not in link.text):
+            version = link.text.split("-")[1].split(".tar.gz")[0].split(".")
+            versionMajor = int(version[0])
+            versionMinor = int(version[1])
+            if len(version) == 2:
+                versionPatch = 0
+            else:
+                versionPatch = int(version[2])
+
+            if versionMajor > maxVersionMajor:
+                maxVersionMajor = versionMajor
+                maxVersionMinor = versionMinor
+                maxVersionPatch = versionPatch
+                versionText = link.text.split("-")[1].split(".tar.gz")[0]
+            elif (versionMajor == maxVersionMajor
+             and versionMinor > maxVersionMinor):
+                maxVersionMinor = versionMinor
+                maxVersionPatch = versionPatch
+                versionText = link.text.split("-")[1].split(".tar.gz")[0]
+            elif (versionMajor == maxVersionMajor
+             and versionMinor == maxVersionMinor
+             and versionPatch > maxVersionPatch):
+                maxVersionPatch = versionPatch
+                versionText = link.text.split("-")[1].split(".tar.gz")[0]
+
+    return versionText
