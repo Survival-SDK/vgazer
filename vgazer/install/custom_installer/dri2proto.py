82e2a57036c0d0963d7219eb78e121334a8de7ac
Vasiliy Edomin <Vasiliy.Edomin@gmail.com>
2019-12-16 02:35:25 +0600

    Add dri2proto custom checker and installer

---
+++ vgazer/install/custom_installer/dri2proto.py
@@ -0,0 +1,83 @@
+import os
+import requests
+from bs4 import BeautifulSoup
+
+from vgazer.command     import RunCommand
+from vgazer.exceptions  import CommandError
+from vgazer.exceptions  import InstallError
+from vgazer.platform    import GetInstallPrefix
+from vgazer.platform    import GetTriplet
+from vgazer.store.temp  import StoreTemp
+from vgazer.working_dir import WorkingDir
+
+def GetTarballUrl():
+    response = requests.get("https://www.x.org/releases/individual/proto/")
+    html = response.content.decode("utf-8")
+    parsedHtml = BeautifulSoup(html, "html.parser")
+
+    links = parsedHtml.find_all("a")
+
+    maxVersionMajor = -1
+    maxVersionMinor = -1
+    maxVersionPatch = -1
+    for link in links:
+        if ("dri2proto-" in link.text and ".tar.gz" in link.text):
+            version = link.text.split("-")[1].split(".tar.gz")[0].split(".")
+            versionMajor = int(version[0])
+            versionMinor = int(version[1])
+            if len(version) == 2:
+                versionPatch = 0
+            else:
+                versionPatch = int(version[2])
+
+            if versionMajor > maxVersionMajor:
+                maxVersionMajor = versionMajor
+                maxVersionMinor = versionMinor
+                maxVersionPatch = versionPatch
+                url = ("https://www.x.org/releases/individual/proto/"
+                 + link["href"])
+            elif (versionMajor == maxVersionMajor
+             and versionMinor > maxVersionMinor):
+                maxVersionMinor = versionMinor
+                maxVersionPatch = versionPatch
+                url = ("https://www.x.org/releases/individual/proto/"
+                 + link["href"])
+            elif (versionMajor == maxVersionMajor
+             and versionMinor == maxVersionMinor
+             and versionPatch > maxVersionPatch):
+                maxVersionPatch = versionPatch
+                url = ("https://www.x.org/releases/individual/proto/"
+                 + link["href"])
+
+    return url
+
+def Install(auth, software, platform, platformData, verbose):
+    installPrefix = GetInstallPrefix(platformData)
+    targetTriplet = GetTriplet(platformData["target"])
+
+    storeTemp = StoreTemp()
+    storeTemp.ResolveEmptySubdirectory(software)
+    tempPath = storeTemp.GetSubdirectoryPath(software)
+
+    tarballUrl = GetTarballUrl()
+    tarballShortFilename = tarballUrl.split("/")[-1]
+
+    try:
+        with WorkingDir(tempPath):
+            RunCommand(["wget", "-P", "./", tarballUrl], verbose)
+            RunCommand(
+             ["tar", "--verbose", "--extract", "--gzip", "--file",
+              tarballShortFilename],
+             verbose)
+        extractedDir = os.path.join(tempPath, tarballShortFilename[0:-7])
+        with WorkingDir(extractedDir):
+            RunCommand(
+             ["./configure", "--host=" + targetTriplet,
+              "--prefix=" + installPrefix],
+             verbose)
+            RunCommand(["make", "install"], verbose)
+    except CommandError:
+        print("VGAZER: Unable to install", software)
+        raise InstallError(software + " not installed")
+
+    print("VGAZER:", software, "installed")
