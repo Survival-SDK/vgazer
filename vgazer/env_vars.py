122792bd599a43c3d1f8497fc205f4c7b5ae453b
Vasiliy Edomin <Vasiliy.Edomin@gmail.com>
2019-12-16 01:46:20 +0600

    Add message by vgazer when env var changed

--- vgazer/env_vars.py
+++ vgazer/env_vars.py
@@ -1,17 +1,24 @@
 import os
 
+def PrintMessage(varName):
+    print("VGAZER: Changed environment variable:", varName)
+    print("VGAZER: New value:", os.environ[varName])
+
 def SetEnvVar(name, value):
     os.environ[name] = value
+    PrintMessage(name)
 
 def AppendEnvVar(name, value):
     if name not in os.environ:
         os.environ[name] = ""
     os.environ[name] = os.environ[name] + value
+    PrintMessage(name)
 
 def PrependEnvVar(name, value):
     if name not in os.environ:
         os.environ[name] = ""
     os.environ[name] = value + os.environ[name]
+    PrintMessage(name)
 
 def AppendEnvVarItem(name, value, separator):
     if name not in os.environ:
@@ -19,6 +26,7 @@ def AppendEnvVarItem(name, value, separator):
     else:
         os.environ[name] = os.environ[name] + separator
     os.environ[name] = os.environ[name] + value
+    PrintMessage(name)
 
 def PrependEnvVarItem(name, value, separator):
     if name not in os.environ:
@@ -26,3 +34,4 @@ def PrependEnvVarItem(name, value, separator):
     else:
         os.environ[name] = separator + os.environ[name]
     os.environ[name] = value + os.environ[name]
+    PrintMessage(name)
