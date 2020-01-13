77b6901d73c617abaa4ab14cd134347dfe72ed9a
Vasiliy Edomin <Vasiliy.Edomin@gmail.com>
2020-01-13 19:29:47 +0600

    Add fallback libpciaccess custom installer (when we can not install from gitlab)

---
+++ vgazer/install/custom_installer/libpciaccess-github.py
@@ -0,0 +1,65 @@
+import os
+
+from vgazer.command         import GetCommandOutputUtf8
+from vgazer.command         import RunCommand
+from vgazer.exceptions      import CommandError
+from vgazer.exceptions      import GithubApiRateLimitExceeded
+from vgazer.exceptions      import InstallError
+from vgazer.github_common   import GithubCheckApiRateLimitExceeded
+from vgazer.platform        import GetInstallPrefix
+from vgazer.platform        import GetTriplet
+from vgazer.store.temp      import StoreTemp
+from vgazer.working_dir     import WorkingDir
+
+def Install(auth, software, platform, platformData, verbose):
+    installPrefix = GetInstallPrefix(platformData)
+    targetTriplet = GetTriplet(platformData["target"])
+
+    storeTemp = StoreTemp()
+    storeTemp.ResolveEmptySubdirectory(software)
+    tempPath = storeTemp.GetSubdirectoryPath(software)
+
+    releases = auth["github"].GetJson(
+     "https://api.github.com/repos/freedesktop/xorg-libpciaccess/tags")
+
+    if GithubCheckApiRateLimitExceeded(releases):
+        raise GithubApiRateLimitExceeded(
+         "Github API rate limit reached while searching last version of "
+         "repo: freedesktop/xorg-libpciaccess"
+        )
+
+    tagNum = 0
+    for tag in tags:
+        if tag["name"] in [
+         "xf86-video-xgi-0.9.1",
+        ]:
+            tagNum = tagNum + 1
+        else:
+            break
+
+    tarballUrl = tags[tagNum]["tarball_url"]
+    tarballShortFilename = tarballUrl.split("/")[-1]
+
+    try:
+        RunCommand(["wget", "-P", "./", tarballUrl], verbose)
+            RunCommand(
+             ["tar", "--verbose", "--extract", "--gzip", "--file",
+              tarballShortFilename],
+             verbose)
+            output = GetCommandOutputUtf8(
+             ["tar", "--list", "--file", tarballShortFilename]
+            )
+        extractedDir = os.path.join(tempPath,
+         output.splitlines()[0].split("/")[0])
+        with WorkingDir(extractedDir):
+            RunCommand(
+             ["./autogen.sh", "--host=" + targetTriplet,
+              "--prefix=" + installPrefix],
+             verbose)
+            RunCommand(["make"], verbose)
+            RunCommand(["make", "install"], verbose)
+    except CommandError:
+        print("VGAZER: Unable to install", software)
+        raise InstallError(software + " not installed")
+
+    print("VGAZER:", software, "installed")
