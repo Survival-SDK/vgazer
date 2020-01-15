593424ec0f7aa67f83e399582c922026cfa4f06b
Vasiliy Edomin <Vasiliy.Edomin@gmail.com>
2020-01-15 20:42:57 +0600

    Fix checking argument for None

--- vgazer/store/home.py
+++ vgazer/store/home.py
@@ -6,7 +6,7 @@ from vgazer.store.base import StoreBase
 class StoreHome(StoreBase):
     def __init__(self, subdirectory=None):
         homeDirPath = GetHomeDirectoryPath();
-        if subdirectory == None:
+        if subdirectory is None:
             subdirectory = GetHideDirectoryPrefix() + "vgazer";
         super().__init__(os.path.join(homeDirPath, subdirectory))
 
