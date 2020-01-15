b7d28895ba36ab1e4506760bdf1da31e5b254010
Vasiliy Edomin <Vasiliy.Edomin@gmail.com>
2020-01-15 20:47:53 +0600

    Add generation of CMake's toolchain files

---
+++ vgazer/config/cmake.py
@@ -0,0 +1,43 @@
+from vgazer.platform    import GetCc
+from vgazer.platform    import GetCxx
+from vgazer.platform    import GetInstallPrefix
+from vgazer.platform    import Platform
+from vgazer.store.temp  import StoreTemp
+
+class ConfigCmake():
+    def __init__(self, platformData):
+        self.storeTemp = StoreTemp(".vgazer/cmake")
+        self.crossFileName = self.storeTemp.GetDirectoryFilePath(
+         "toolchain.cmake")
+        self.storeTemp.ResolveDirectory()
+        self.platformData = platformData
+
+    def GetCrossFileName(self):
+        return self.crossFileName
+
+    def GenerateCrossFile(self):
+        print("VGAZER: Generating CMake toolchain file...")
+        cmakeOs = Platform.GetGenericOs(
+          self.platformData["target"].GetOs()
+         ).capitalize()
+        prefix = GetInstallPrefix(self.platformData)
+
+        cc = GetCc(self.platformData["target"])
+        cxx = GetCxx(self.platformData["target"])
+
+        self.storeTemp.DirectoryWriteTextFile("toolchain.cmake",
+         "set(CMAKE_SYSTEM_NAME {cmakeOs})\n"
+         "set(CMAKE_SYSTEM_PROCESSOR {cpu})\n"
+         "set(CMAKE_C_COMPILER {cc})\n"
+         "set(CMAKE_CXX_COMPILER {cxx})\n"
+         "set(CMAKE_FIND_ROOT_PATH {prefix})\n"
+         "SET(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)\n"
+         "SET(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)\n"
+         "SET(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)".format(
+          cmakeOs=cmakeOs,
+          cpu=self.platformData["target"].GetArch(),
+          cc=cc,
+          cxx=cxx,
+          prefix=prefix
+         )
+        )
