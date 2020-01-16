f625f655339575883d2c9a2fbdeec910b53a72eb
Vasiliy Edomin <Vasiliy.Edomin@gmail.com>
2020-01-16 19:50:22 +0600

    Add custom installers for libpcre and libpcre2

---
+++ vgazer/install/custom_installer/libpcre2.py
@@ -0,0 +1,48 @@
+import os
+import requests
+
+from vgazer.command         import RunCommand
+from vgazer.exceptions      import CommandError
+from vgazer.exceptions      import InstallError
+from vgazer.install.utils   import SourceforgeDownloadTarballWhileErrorcodeFour
+from vgazer.platform        import GetInstallPrefix
+from vgazer.platform        import GetTriplet
+from vgazer.store.temp      import StoreTemp
+from vgazer.working_dir     import WorkingDir
+
+def Install(auth, software, platform, platformData, mirrors, verbose):
+    installPrefix = GetInstallPrefix(platformData)
+    targetTriplet = GetTriplet(platformData["target"])
+
+    storeTemp = StoreTemp()
+    storeTemp.ResolveEmptySubdirectory(software)
+    tempPath = storeTemp.GetSubdirectoryPath(software)
+
+    sourceforgeMirrorsManager = mirrors["sourceforge"].CreateMirrorsManager(
+     ["https", "http"])
+
+    filename = requests.get(
+     "https://sourceforge.net/projects/pcre/best_release.json"
+    ).json()["release"]["filename"]
+    arhiveShortFilename = filename.split("/")[-1]
+
+    try:
+        with WorkingDir(tempPath):
+            SourceforgeDownloadTarballWhileErrorcodeFour(
+             sourceforgeMirrorsManager, "pcre", filename, verbose)
+            RunCommand(["unzip", arhiveShortFilename], verbose)
+        extractedDir = os.path.join(tempPath, arhiveShortFilename[0:-4])
+        with WorkingDir(extractedDir):
+            RunCommand(
+             ["./configure", "--prefix=" + installPrefix,
+              "--host=" + targetTriplet, "--enable-pcre2-16",
+              "--enable-pcre2-32", "--enable-jit=auto",
+              "--enable-newline-is-any"],
+             verbose)
+            RunCommand(["make"], verbose)
+            RunCommand(["make", "install"], verbose)
+    except CommandError:
+        print("VGAZER: Unable to install", software)
+        raise InstallError(software + " not installed")
+
+    print("VGAZER:", software, "installed")
