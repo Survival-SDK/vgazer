b3c6885c776aa898d2fc5d19980e7a062c9107ae
Vasiliy Edomin <Vasiliy.Edomin@gmail.com>
2020-01-18 02:04:24 +0600

    Update bzip2 installer

--- vgazer/install/custom_installer/bzip2.py
+++ vgazer/install/custom_installer/bzip2.py
@@ -5,12 +5,18 @@ from vgazer.command         import RunCommand
 from vgazer.exceptions      import CommandError
 from vgazer.exceptions      import InstallError
 from vgazer.install.utils   import SourceforgeDownloadTarballWhileErrorcodeFour
+from vgazer.platform        import GetAr
+from vgazer.platform        import GetCc
 from vgazer.platform        import GetInstallPrefix
+from vgazer.platform        import GetRanlib
 from vgazer.store.temp      import StoreTemp
 from vgazer.working_dir     import WorkingDir
 
 def Install(auth, software, platform, platformData, mirrors, verbose):
     installPrefix = GetInstallPrefix(platformData)
+    cc = GetCc(platformData["target"])
+    ar = GetAr(platformData["target"])
+    ranlib = GetRanlib(platformData["target"])
 
     storeTemp = StoreTemp()
     storeTemp.ResolveEmptySubdirectory(software)
@@ -35,12 +41,17 @@ def Install(auth, software, platform, platformData, mirrors, verbose):
         extractedDir = os.path.join(tempPath, tarballShortFilename[0:-7])
         with WorkingDir(extractedDir):
             RunCommand(
-             ["make",
+             ["make", "CC=" + cc, "AR=" + ar, "RANLIB=" + ranlib,
               "CFLAGS=-Wall -Winline -O2 -g -D_FILE_OFFSED_BITS=64 -fPIC"],
              verbose)
             RunCommand(["make", "install", "PREFIX=" + installPrefix], verbose)
-            RunCommand(["make", "-f", "Makefile-libbz2_so"], verbose)
-            RunCommand(["cp", "./libbz2.so.1.0.6", installPrefix + "/lib"],
+            RunCommand(
+             ["make", "-f", "Makefile-libbz2_so", "CC=" + cc],
+             verbose)
+            RunCommand(
+             ["sh", "-c", "ln -s ./libbz2.so.?.? ./libbz2.so"],
+             verbose)
+            RunCommand(["cp", "./libbz2.so", installPrefix + "/lib"],
              verbose)
     except CommandError:
         print("VGAZER: Unable to install", software)
