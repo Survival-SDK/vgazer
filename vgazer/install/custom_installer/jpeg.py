936dc3e76a8ddb9d9faa6e56699f27a5abb628f8
Vasiliy Edomin <Vasiliy.Edomin@gmail.com>
2020-01-19 21:49:42 +0600

    Add custom installers for libogg, libmodplug, libflac and libjpeg

---
+++ vgazer/install/custom_installer/jpeg.py
@@ -0,0 +1,54 @@
+import os
+import requests
+
+from vgazer.command     import RunCommand
+from vgazer.exceptions  import CommandError
+from vgazer.exceptions  import InstallError
+from vgazer.platform    import GetInstallPrefix
+from vgazer.platform    import GetTriplet
+from vgazer.store.temp  import StoreTemp
+from vgazer.working_dir import WorkingDir
+
+def GetVersion():
+    response = requests.get("http://www.ijg.org/")
+    html = response.content.decode("utf-8")
+    lines = html.splitlines()
+    for line in lines:
+        if line.startswith("The current version is release"):
+            return line.split(" ")[5]
+
+def Install(auth, software, platform, platformData, mirrors, verbose):
+    installPrefix = GetInstallPrefix(platformData)
+    targetTriplet = GetTriplet(platformData["target"])
+
+    storeTemp = StoreTemp()
+    storeTemp.ResolveEmptySubdirectory(software)
+    tempPath = storeTemp.GetSubdirectoryPath(software)
+
+    version = GetVersion()
+
+    tarballUrl = "http://www.ijg.org/files/jpegsrc.v" + version + ".tar.gz"
+    tarballShortFilename = tarballUrl.split("/")[-1]
+
+    try:
+        with WorkingDir(tempPath):
+            RunCommand(
+             ["wget", "-P", "./", "-O", tarballShortFilename, tarballUrl],
+             verbose)
+            RunCommand(
+             ["tar", "--verbose", "--extract", "--gzip", "--file",
+              tarballShortFilename],
+             verbose)
+        extractedDir = os.path.join(tempPath, "jpeg-" + version)
+        with WorkingDir(extractedDir):
+            RunCommand(
+             ["./configure", "--host=" + targetTriplet,
+              "--prefix=" + installPrefix],
+             verbose)
+            RunCommand(["make"], verbose)
+            RunCommand(["make", "install"], verbose)
+    except CommandError:
+        print("VGAZER: Unable to install", software)
+        raise InstallError(software + " not installed")
+
+    print("VGAZER:", software, "installed")
