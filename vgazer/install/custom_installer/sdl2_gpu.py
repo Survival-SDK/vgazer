489f0eb83b3ba8f77ed32e112ea965cb6d68d818
Vasiliy Edomin <Vasiliy.Edomin@gmail.com>
2020-01-29 23:55:22 +0600

    Update SDL2_gpu custom installer

--- vgazer/install/custom_installer/sdl2_gpu.py
+++ vgazer/install/custom_installer/sdl2_gpu.py
@@ -1,21 +1,37 @@
 import os
 
-from vgazer.command     import RunCommand
-from vgazer.env_vars    import SetEnvVar
-from vgazer.exceptions  import CommandError
-from vgazer.exceptions  import InstallError
-from vgazer.platform    import GetAr
-from vgazer.platform    import GetCc
-from vgazer.platform    import GetCxx
-from vgazer.platform    import GetInstallPrefix
-from vgazer.store.temp  import StoreTemp
-from vgazer.working_dir import WorkingDir
+from vgazer.command         import RunCommand
+from vgazer.config.cmake    import ConfigCmake
+from vgazer.exceptions      import CommandError
+from vgazer.exceptions      import InstallError
+from vgazer.platform        import GetAr
+from vgazer.platform        import GetCc
+from vgazer.platform        import GetInstallPrefix
+from vgazer.platform        import GetTriplet
+from vgazer.store.temp      import StoreTemp
+from vgazer.working_dir     import WorkingDir
+
+def GetVersionFromSource(filename):
+    with open(filename) as f:
+        lines = f.readlines()
+    for line in lines:
+        if "#define SDL_GPU_VERSION_MAJOR" in line:
+            versionMajor = line.split(" ")[2]
+        if "#define SDL_GPU_VERSION_MINOR" in line:
+            versionMinor = line.split(" ")[2]
+        if "#define SDL_GPU_VERSION_PATCH" in line:
+            versionPatch = line.split(" ")[2]
+
+    return "{major}.{minor}.{patch}".format(major=versionMajor,
+     minor=versionMinor, patch=versionPatch)
 
 def Install(auth, software, platform, platformData, mirrors, verbose):
+    configCmake = ConfigCmake(platformData)
+    configCmake.GenerateCrossFile()
     installPrefix = GetInstallPrefix(platformData)
+    targetTriplet = GetTriplet(platformData["target"])
     ar = GetAr(platformData["target"])
     cc = GetCc(platformData["target"])
-    cxx = GetCxx(platformData["target"])
 
     storeTemp = StoreTemp()
     storeTemp.ResolveEmptySubdirectory(software)
@@ -30,10 +46,10 @@ def Install(auth, software, platform, platformData, mirrors, verbose):
         clonedDir = os.path.join(tempPath, "sdl2-gpu")
         with WorkingDir(clonedDir):
             RunCommand(["mkdir", "build"], verbose)
+        sdlGpuHeader = os.path.join(clonedDir, "include/SDL_gpu.h")
+        version = GetVersionFromSource(sdlGpuHeader)
         buildDir = os.path.join(clonedDir, "build")
         with WorkingDir(buildDir):
-            SetEnvVar("CC", cc)
-            SetEnvVar("CXX", cxx)
             RunCommand(
              [cc, "-c", "../src/externals/stb_image/stb_image.c",
               "-o", "../src/externals/stb_image/stb_image.o", "-O2", "-Wall",
@@ -56,20 +72,36 @@ def Install(auth, software, platform, platformData, mirrors, verbose):
              verbose)
             RunCommand(
              ["cmake", "..", "-G", "Unix Makefiles",
+              "-DCMAKE_TOOLCHAIN_FILE=" + configCmake.GetCrossFileName(),
               "-DCMAKE_INSTALL_PREFIX=" + installPrefix,
-              "-DSDL_gpu_BUILD_DEMOS=OFF", "-DSDL_gpu_USE_SYSTEM_GLEW=ON",
+              "-DSDL_gpu_BUILD_DEMOS=OFF", #"-DSDL_gpu_DISABLE_GLES=OFF",
+              "-DSDL_gpu_USE_SYSTEM_GLEW=ON",
               "-DSTBI_INCLUDE_DIR=" + installPrefix + "/include",
               "-DSTBI_LIBRARY=" + buildDir
                + "/../src/externals/stb_image/libstbi.a",
-              "-DSTBI_FOUND=TRUE"
+              "-DSTBI_FOUND=TRUE",
               "-DSTBI_WRITE_INCLUDE_DIR=" + installPrefix + "/include",
               "-DSTBI_WRITE_LIBRARY=" + buildDir
                + "/../src/externals/stb_image_write/libstbi_write.a",
-              "-DSTBI_WRITE_FOUND=TRUE"
+              "-DSTBI_WRITE_FOUND=TRUE",
+              "-DM_LIB=/usr/lib/" + targetTriplet + "/libm.so"
              ],
             verbose)
             RunCommand(["make"], verbose)
             RunCommand(["make", "install"], verbose)
+            RunCommand(
+             ["mv",
+              installPrefix + "/SDL_gpu-" + version + "/lib/libSDL2_gpu.so",
+              installPrefix + "/lib/libSDL2_gpu.so"],
+             verbose)
+            RunCommand(
+             ["mv",
+              installPrefix + "/SDL_gpu-" + version + "/lib/libSDL2_gpu.a",
+              installPrefix + "/lib/libSDL2_gpu.a"],
+             verbose)
+            RunCommand(
+             ["rm", "-f", installPrefix + "/SDL_gpu-" + version],
+             verbose)
     except CommandError:
         print("VGAZER: Unable to install", software)
         raise InstallError(software + " not installed")
