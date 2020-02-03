1046ef93fa74d4314c1abb7be8ce41afe85d49bb
Vasiliy Edomin <Vasiliy.Edomin@gmail.com>
2020-02-04 02:53:38 +0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,29 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
- name='vgazer',
- version='0.0',
- url='https://github.com/edomin/vgazer',
- license='CC0',
- author='Vasiliy Edomin',
- author_email='Vasiliy.Edomin@gmail.com',
- description='Tool for checking versions of various software',
+ name="vgazer",
+ version="0.0.0",
+ url="https://github.com/edomin/vgazer",
+ license="CC0",
+ author="Vasiliy Edomin",
+ author_email="Vasiliy.Edomin@gmail.com",
+ description="Library for checking versions and installing various software",
+ long_description=long_description,
+ long_description_content_type="text/markdown",
  packages=find_packages(exclude=["samples"]),
- long_description=open('README.md').read(),
+ install_requires=[
+  "requests",
+  "bs4",
+ ],
+ classifiers=[
+  "Topic :: Software Development :: Build Tools",
+  "Programming Language :: Python :: 3",
+  "License :: CC0 1.0 Universal (CC0 1.0) Public Domain Dedication",
+  "Operating System :: POSIX :: Linux",
+  "Natural Language :: English",
+ ],
  zip_safe=False
 )
