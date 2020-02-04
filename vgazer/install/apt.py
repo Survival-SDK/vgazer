23d74dcefb50f036794bd8a389338f17bd760ae4
Vasiliy Edomin <Vasiliy.Edomin@gmail.com>
2020-02-04 22:47:50 +0600

    Add possibility to install multiple packages in one installer call for apt installer

--- vgazer/install/apt.py
+++ vgazer/install/apt.py
@@ -2,7 +2,7 @@ from vgazer.command     import RunCommand
 from vgazer.exceptions  import CommandError
 from vgazer.exceptions  import InstallError
 
-def InstallAptFromBackport(software, package, hostPlatform,
+def InstallPackageFromBackport(software, package, hostPlatform,
  showInstalledMessage, verbose):
     try:
         RunCommand(
@@ -16,15 +16,25 @@ def InstallAptFromBackport(software, package, hostPlatform,
     if showInstalledMessage:
         print("VGAZER:", software, "installed")
 
-def InstallApt(software, package, hostPlatform, showInstalledMessage, verbose):
+def InstallPackage(software, package, hostPlatform, showInstalledMessage,
+ verbose):
     try:
         RunCommand(["apt-get", "install", "-y", package], verbose)
     except CommandError as e:
         if e.errorcode == 100 and hostPlatform.GetOs() == "debian":
-            InstallAptFromBackport(software, package, hostPlatform, verbose)
+            InstallPackageFromBackport(software, package, hostPlatform,
+             showInstalledMessage, verbose)
         else:
             print("VGAZER: Unable to install", software)
             raise InstallError(software + " not installed")
 
     if showInstalledMessage:
         print("VGAZER:", software, "installed")
+
+def InstallApt(software, packages, hostPlatform, verbose):
+    if isinstance(packages, list):
+        for package in packages:
+            InstallPackage(software, package, hostPlatform,
+             package is packages[-1], verbose)
+    else:
+        InstallPackage(software, packages, hostPlatform, True, verbose)
