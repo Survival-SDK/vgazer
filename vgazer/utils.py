0acefdae4fa95c75e5e698964c6f16a6ff7e6e15
Vasiliy Edomin <Vasiliy.Edomin@gmail.com>
2020-02-13 11:26:22 +0600

    Add 2 utility functions: NewListWithReplace and FindFileInDir

--- vgazer/utils.py
+++ vgazer/utils.py
@@ -1,4 +1,20 @@
+import os
+
+from vgazer.exceptions  import FileNotFound
+
 def GetKeyByValue(dictionary, value):
     keys = list(dictionary.keys())
     values = list(dictionary.values())
     return keys[values.index(value)]
+
+def NewListWithReplace(array, oldText, newText):
+    for element in array:
+        array.replace(oldText, newText)
+
+def FindFileInDir(directories, filenames):
+    for directory in directories:
+        for filename in filenames:
+            if os.path.isfile(os.path.join(directory, filename)):
+                return filename
+    raise FileNotFound(
+     "Unable to find any file from list: " + ", ".join(filenames))
