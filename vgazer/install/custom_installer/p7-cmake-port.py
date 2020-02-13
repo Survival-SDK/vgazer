5e3f0033cbc2a7fcb719a5fecc237abfcbc1b18d
Vasiliy Edomin <Vasiliy.Edomin@gmail.com>
2020-02-13 11:33:30 +0600

    Add p7 cmake port custom installer
    Currently not used but may be used as fallback installer

---
+++ vgazer/install/custom_installer/p7-cmake-port.py
@@ -0,0 +1,65 @@
+import os
+
+from vgazer.command         import GetCommandOutputUtf8
+from vgazer.command         import RunCommand
+from vgazer.config.cmake    import ConfigCmake
+from vgazer.exceptions      import CommandError
+from vgazer.exceptions      import GithubApiRateLimitExceeded
+from vgazer.exceptions      import InstallError
+from vgazer.github_common   import GithubCheckApiRateLimitExceeded
+from vgazer.platform        import GetInstallPrefix
+from vgazer.store.temp      import StoreTemp
+from vgazer.working_dir     import WorkingDir
+
+def Install(auth, software, platform, platformData, mirrors, verbose):
+    configCmake = ConfigCmake(platformData)
+    configCmake.GenerateCrossFile()
+
+    installPrefix = GetInstallPrefix(platformData)
+
+    storeTemp = StoreTemp()
+    storeTemp.ResolveEmptySubdirectory(software)
+    tempPath = storeTemp.GetSubdirectoryPath(software)
+
+    releases = auth["github"].GetJson(
+     "https://api.github.com/repos/CMakePorts/libp7-baical/releases")
+
+    if GithubCheckApiRateLimitExceeded(releases):
+        raise GithubApiRateLimitExceeded(
+         "Github API rate limit reached while searching last version of "
+         "repo: CMakePorts/libp7-baical"
+        )
+
+    tarballUrl = releases[0]["tarball_url"]
+    tarballShortFilename = tarballUrl.split("/")[-1]
+
+    try:
+        with WorkingDir(tempPath):
+            RunCommand(["wget", "-P", "./", tarballUrl], verbose)
+            RunCommand(
+             ["tar", "--verbose", "--extract", "--gzip", "--file",
+              tarballShortFilename],
+             verbose)
+            output = GetCommandOutputUtf8(
+             ["tar", "--list", "--file", tarballShortFilename]
+            )
+        extractedDir = os.path.join(tempPath,
+         output.splitlines()[0].split("/")[0])
+        with WorkingDir(extractedDir):
+            RunCommand(["mkdir", "build"], verbose)
+        buildDir = os.path.join(extractedDir, "build")
+        with WorkingDir(buildDir):
+            RunCommand(
+             ["cmake", "..",
+              "-DCMAKE_TOOLCHAIN_FILE=" + configCmake.GetCrossFileName(),
+              "-DCMAKE_INSTALL_PREFIX=" + installPrefix,
+              "-DCMAKE_VERBOSE_MAKEFILE:BOOL=ON", "-DBUILD_SHARED_LIBS=ON",
+              "-DBUILD_TESTS=OFF", "-DBUILD_EXAMPLES=OFF"],
+             verbose)
+            RunCommand(["make"], verbose)
+            RunCommand(["make", "install"], verbose)
+    except CommandError:
+        print("VGAZER: Unable to install", software)
+        raise InstallError(software + " not installed")
+
+    print("VGAZER:", software, "installed")
