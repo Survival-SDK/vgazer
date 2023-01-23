041e9aac463ef1eb559621ebced6852b5035e873
Vasiliy Edomin <Vasiliy.Edomin@gmail.com>
2023-01-23 16:19:46 +0700

    Add installer for latest version of libsir

---
+++ vgazer/install/custom_installer/libsir-master.py
@@ -0,0 +1,46 @@
+import os
+
+from vgazer.command         import GetCommandOutputUtf8
+from vgazer.command         import RunCommand
+from vgazer.exceptions      import CommandError
+from vgazer.exceptions      import GithubApiRateLimitExceeded
+from vgazer.exceptions      import InstallError
+from vgazer.github_common   import GithubCheckApiRateLimitExceeded
+from vgazer.platform        import GetAr
+from vgazer.platform        import GetCc
+from vgazer.platform        import GetInstallPrefix
+from vgazer.store.temp      import StoreTemp
+from vgazer.working_dir     import WorkingDir
+
+def Install(auth, software, platform, platformData, mirrors, verbose):
+    installPrefix = GetInstallPrefix(platformData)
+    ar = GetAr(platformData["target"])
+    cc = GetCc(platformData["target"])
+
+    storeTemp = StoreTemp()
+    storeTemp.ResolveEmptySubdirectory(software)
+    tempPath = storeTemp.GetSubdirectoryPath(software)
+
+    try:
+        with WorkingDir(tempPath):
+            RunCommand(
+             ["git", "clone", "https://github.com/ryanlederman/libsir.git"],
+             verbose)
+        clonedDir = os.path.join(tempPath, "libsir")
+        with WorkingDir(clonedDir):
+            RunCommand(["make", "static", "CC=" + cc, "AR=" + ar], verbose)
+            if not os.path.exists(installPrefix + "/include"):
+                RunCommand(["mkdir", "-p", installPrefix + "/include"],
+                 verbose)
+            if not os.path.exists(installPrefix + "/lib"):
+                RunCommand(["mkdir", "-p", installPrefix + "/lib"], verbose)
+            RunCommand(["sh", "-c", "cp ./*.h " + installPrefix + "/include"],
+             verbose)
+            RunCommand(
+             ["cp", "./build/lib/libsir_s.a", installPrefix + "/lib"],
+             verbose)
+    except CommandError:
+        print("VGAZER: Unable to install", software)
+        raise InstallError(software + " not installed")
+
+    print("VGAZER:", software, "installed")
