66d6cb95dd527e92eae87a068e4af8e677157c41
Vasiliy Edomin <vasiliy.edomin@gmail.com>
2023-03-30 21:36:41 +0700

    Fix codestyle

--- vgazer/install/custom_installer/luajit.py
+++ vgazer/install/custom_installer/luajit.py
@@ -45,16 +45,23 @@ def Install(auth, software, platform, platformData, mirrors, verbose):
              ["tar", "--verbose", "--extract", "--gzip", "--file",
               tarballShortFilename],
              verbose)
-        extractedDir = os.path.join(tempPath,
-         tarballShortFilename[0:-7])
+        extractedDir = os.path.join(tempPath, tarballShortFilename[0:-7])
         with WorkingDir(extractedDir):
             RunCommand(
-             ["make", "BUILDMODE=static", "CROSS=" + triplet + "-",
-              "TARGET_SYS=" + platformData["target"].GetOs().capitalize()],
+             [
+              "make", "BUILDMODE=static",
+              "CROSS={triplet}-".format(triplet=triplet),
+              "TARGET_SYS={os}".format(
+               os=platformData["target"].GetOs().capitalize())
+             ],
              verbose)
-            RunCommand(["make", "install", "PREFIX=" + installPrefix], verbose)
+            RunCommand(
+             ["make", "install",
+              "PREFIX={prefix}".format(prefix=installPrefix)
+             ],
+             verbose)
     except CommandError:
         print("VGAZER: Unable to install", software)
-        raise InstallError(software + " not installed")
+        raise InstallError("{software} not installed".format(software=software))
 
     print("VGAZER:", software, "installed")
