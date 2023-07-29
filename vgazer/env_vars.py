c157bd5df2aa77bee3c52d29346c0ecab98583e7
Vasiliy Edomin <Vasiliy.Edomin@gmail.com>
2023-07-29 12:09:22 +0700

    Remove functions AppendEnvVar, PrependEnvVar, AppendEnvVarItem and PrependEnvVarItem

--- vgazer/env_vars.py
+++ vgazer/env_vars.py
@@ -8,30 +8,6 @@ def SetEnvVar(name, value):
     os.environ[name] = value
     PrintMessage(name)
 
-def AppendEnvVar(name, value):
-    if name not in os.environ:
-        os.environ[name] = ""
-    os.environ[name] = os.environ[name] + value
-    PrintMessage(name)
 
-def PrependEnvVar(name, value):
-    if name not in os.environ:
-        os.environ[name] = ""
-    os.environ[name] = value + os.environ[name]
-    PrintMessage(name)
 
-def AppendEnvVarItem(name, value, separator):
-    if name not in os.environ:
-        os.environ[name] = ""
-    else:
-        os.environ[name] = os.environ[name] + separator
-    os.environ[name] = os.environ[name] + value
-    PrintMessage(name)
 
-def PrependEnvVarItem(name, value, separator):
-    if name not in os.environ:
-        os.environ[name] = ""
-    else:
-        os.environ[name] = separator + os.environ[name]
-    os.environ[name] = value + os.environ[name]
-    PrintMessage(name)
