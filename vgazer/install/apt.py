e9020e8823b833d9eae5610e8001ed694be9834c
Vasiliy Edomin <Vasiliy.Edomin@gmail.com>
2023-07-29 13:39:09 +0700

    Fix apt installer for try to install from debian backports before regular repo

--- vgazer/install/apt.py
+++ vgazer/install/apt.py
@@ -2,26 +2,35 @@ from vgazer.command     import RunCommand
 from vgazer.exceptions  import CommandError
 from vgazer.exceptions  import InstallError
 
-def InstallPackageFromBackport(software, package, hostPlatform, verbose):
+def InstallPackageDebian(software, package, hostPlatform, verbose):
     try:
         RunCommand(
-         ["apt-get", "-t", hostPlatform.GetOsVersion() + "-backports",
-          "install", "-y", package],
+         ["apt-get", "-t",
+          "{osver}-backports".format(osver=hostPlatform.GetOsVersion()),
+          "install", "-y", package
+         ],
          verbose)
-    except CommandError:
-        print("VGAZER: Unable to install", software)
-        raise InstallError(software + " not installed")
-
-def InstallPackage(software, package, hostPlatform, verbose):
-    try:
-        RunCommand(["apt-get", "install", "-y", package], verbose)
     except CommandError as e:
-        if e.errorcode == 100 and hostPlatform.GetOs() == "debian":
-            InstallPackageFromBackport(software, package, hostPlatform,
-             verbose)
+        if e.errorcode == 100:
+            InstallPackageOther(software, package, hostPlatform, verbose)
         else:
             print("VGAZER: Unable to install", software)
-            raise InstallError(software + " not installed")
+            raise InstallError(
+             "{software} not installed".format(software=software))
+
+def InstallPackageOther(software, package, hostPlatform, verbose):
+    try:
+        RunCommand(["apt-get", "install", "-y", package], verbose)
+    except CommandError:
+        print("VGAZER: Unable to install", software)
+        raise InstallError(
+         "{software}  not installed".format(software=software))
+
+def InstallPackage(software, package, hostPlatform, verbose):
+    if hostPlatform.GetOs() == "debian":
+        InstallPackageDebian(software, package, hostPlatform, verbose);
+    else:
+        InstallPackageOther(software, package, hostPlatform, verbose);
 
 def InstallApt(software, packages, postInstallCommands, hostPlatform, verbose):
     if isinstance(packages, list):
