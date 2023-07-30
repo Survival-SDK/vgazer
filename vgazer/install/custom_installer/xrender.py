cd533d496c818772c38c48b4ee313395fbe676a2
Vasiliy Edomin <Vasiliy.Edomin@gmail.com>
2023-07-30 20:40:33 +0700

    Update software data and installer for xrender
    Because freedesktop.org removed github repo

--- vgazer/install/custom_installer/xrender.py
+++ vgazer/install/custom_installer/xrender.py
@@ -2,10 +2,9 @@ import os
 
 from vgazer.command              import GetCommandOutputUtf8
 from vgazer.command              import RunCommand
+from vgazer.env_vars             import EnvVar
 from vgazer.exceptions           import CommandError
-from vgazer.exceptions           import GithubApiError
 from vgazer.exceptions           import InstallError
-from vgazer.github_api_error_mgr import GithubApiErrorMgr
 from vgazer.platform             import GetInstallPrefix
 from vgazer.platform             import GetTriplet
 from vgazer.store.temp           import StoreTemp
@@ -19,38 +18,13 @@ def Install(auth, software, platform, platformData, mirrors, verbose):
     storeTemp.ResolveEmptySubdirectory(software)
     tempPath = storeTemp.GetSubdirectoryPath(software)
 
-    try:
-        tags = auth["github"].GetJson(
-         "https://api.github.com/repos/freedesktop/xorg-libXrender/tags")
-    except ConnectionError:
-        print("VGAZER: Unable to know last version of", software)
-        raise InstallError(software + " not installed")
+    tags = auth["base"].GetJson(
+     "https://gitlab.freedesktop.org/api/v4/projects/730/repository/tags")
 
-    with GithubApiErrorMgr(tags, "freedesktop/xorg-libXrender") as errMgr:
-        if errMgr.IsErrorOccured():
-            raise GithubApiError(errMgr.GetErrorText())
-
-    tagNum = 0
-    for tag in tags:
-        if tag["name"] in [
-         "xo-6_7_0",
-         "xf86-012804-2330",
-         "xf86-4_4_99_1",
-         "xf86-4_4_0",
-         "xf86-4_3_99_903",
-         "xf86-4_3_99_903_special",
-         "xf86-4_3_99_902",
-         "xf86-4_3_99_901",
-         "xf86-4_3_99_16",
-         "xf86-4_3_0_1",
-         "sco_port_update-base",
-         "rel-0-6-1",
-        ]:
-            tagNum = tagNum + 1
-        else:
-            break
-
-    tarballUrl = tags[tagNum]["tarball_url"]
+    tarballUrl = (
+     "https://gitlab.freedesktop.org/api/v4/projects/730/repository/"
+     "archive.tar.gz?sha=" + tags[0]["name"]
+    )
     tarballShortFilename = tarballUrl.split("/")[-1]
 
     try:
@@ -65,7 +39,9 @@ def Install(auth, software, platform, platformData, mirrors, verbose):
             )
         extractedDir = os.path.join(tempPath,
          output.splitlines()[0].split("/")[0])
-        with WorkingDir(extractedDir):
+        with (WorkingDir(extractedDir),
+         EnvVar("ACLOCAL", "aclocal -I {prefix}/share/aclocal".format(
+          prefix=installPrefix))):
             RunCommand(
              ["./autogen.sh", "--host=" + targetTriplet,
               "--prefix=" + installPrefix,
