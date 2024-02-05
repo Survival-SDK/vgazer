ef522e9f5b8d912a06cdfe784cb10985d9a35e8a
Vasiliy Edomin <Vasiliy.Edomin@gmail.com>
2024-02-06 01:08:42 +0600

    Add verbose parameter for WorkingDir

--- libvgazer/working_dir.py
+++ libvgazer/working_dir.py
@@ -3,14 +3,18 @@ from pathlib import Path
 
 class WorkingDir:
     """Context manager for changing the current working directory"""
-    def __init__(self, newPath):
+    def __init__(self, newPath, verbose=True):
         self.newPath = os.path.expanduser(newPath)
+        self.verbose = verbose
 
     def __enter__(self):
         self.savedPath = os.getcwd()
-        print("VGAZER: Entering to directory:", self.newPath)
         Path(self.newPath).mkdir(parents=True, exist_ok=True)
+        if self.verbose:
+            print("VGAZER: Entering to directory:", self.newPath)
         os.chdir(self.newPath)
 
     def __exit__(self, etype, value, traceback):
+        if self.verbose:
+            print("VGAZER: Returning to directory:", self.savedPath)
         os.chdir(self.savedPath)
