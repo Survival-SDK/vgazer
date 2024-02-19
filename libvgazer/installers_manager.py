c30fd196b4d7067bd379b33b6458ed64690ba289
Vasiliy Edomin <Vasiliy.Edomin@gmail.com>
2024-02-19 16:48:12 +0600

    Remove pip2 installer

--- libvgazer/installers_manager.py
+++ libvgazer/installers_manager.py
@@ -2,7 +2,6 @@ from libvgazer.exceptions     import UnknownInstaller
 from libvgazer.install.apt    import InstallApt
 from libvgazer.install.cmd    import InstallCmd
 from libvgazer.install.pacman import InstallPacman
-from libvgazer.install.pip    import InstallPip
 from libvgazer.install.pip3   import InstallPip3
 from libvgazer.install.yum    import InstallYum
 
@@ -22,8 +21,6 @@ class InstallersManager:
                 True,
             "pacman": lambda software, platformData, installerData, verbose:
                 InstallPacman(software, installerData["package"], verbose),
-            "pip": lambda software, platformData, installerData, verbose:
-                InstallPip(software, installerData["package"], verbose),
             "pip3": lambda software, platformData, installerData,
              verbose:
                 InstallPip3(software, installerData["package"], verbose),
